0e3f3652a7d11b585dd72e6ac0d05ee07c04f853
hongocthaovy0710 <hongocthaovy66@gmail.com>
2024-11-21 14:06:02 +0700

    xong phần vy rồi nha

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,13 +16,19 @@ class ProductController extends Controller
         return view('admin.add_product')->with('cate_product', $cate_product)->with('brand_product', $brand_product);
     }
 
-    public function all_product() {
-        $all_product = DB::table('tbl_product')->get();
+    public function all_product()
+    {
+        $this->AuthLogin();
+        $all_product = DB::table('tbl_product')
+            ->join('tbl_category_product', 'tbl_category_product.category_id', '=', 'tbl_product.category_id')
+            ->join('tbl_brand', 'tbl_brand.brand_id', '=', 'tbl_product.brand_id')
+            ->orderBy('tbl_product.product_id', 'desc')->get();
         $manager_product = view('admin.all_product')->with('all_product', $all_product);
         return view('admin_layout')->with('admin.all_product', $manager_product);
     }
 
-    public function save_product(Request $request){
+    public function save_product(Request $request)
+    {
         $this->AuthLogin();
         $data = array();
         $data['product_name'] = $request->product_name;
@@ -32,6 +38,8 @@ class ProductController extends Controller
         $data['category_id'] = $request->product_cate;
         $data['brand_id'] = $request->product_brand;
         $data['product_status'] = $request->product_status;
+        $date['product_image'] = $request->product_status;
+
         $get_image = $request->file('product_image');
         if ($get_image) {
             $get_name_image = $get_image->getClientOriginalName();
@@ -40,9 +48,10 @@ class ProductController extends Controller
             $get_image->move('public/uploads/product', $new_image);
             $data['product_image'] = $new_image;
         }
+
         DB::table('tbl_product')->insert($data);
-        Session::put('message', 'Thêm danh mục sản phẩm thành công');
-        return Redirect::to('/add-product');
+        Session::put('message', 'Thêm sản phẩm thành công');
+        return Redirect::to('/all-product');
     }
 
     public function AuthLogin(){
@@ -65,20 +74,44 @@ class ProductController extends Controller
         Session::put('message', 'Kích hoạt danh mục sản phẩm thành công');
         return Redirect::to('all-product');
     }
-    public function edit_product($product_id){
-        $edit_product = DB::table('tbl_product')->where('product_id',$product_id)->get();
-        $manager_product = view('admin.edit_product')->with('edit_product', $edit_product);
+    public function edit_product($product_id)
+    {
+        $this->AuthLogin();
+        $cate_product = DB::table('tbl_category_product')->orderby('category_id', 'desc')->get();
+        $brand_product = DB::table('tbl_brand')->orderby('brand_id', 'desc')->get();
+
+        $edit_product = DB::table('tbl_product')->where('product_id', $product_id)->get();
+        $manager_product = view('admin.edit_product')->with('edit_product', $edit_product)
+            ->with('cate_product', $cate_product)->with('brand_product', $brand_product);
         return view('admin_layout')->with('admin.edit_product', $manager_product);
     }
 
-    public function update_product(Request $request,$product_id){
+    
+
+    public function update_product(Request $request, $product_id)
+    {
+        $this->AuthLogin();
         $data = array();
         $data['product_name'] = $request->product_name;
+        $data['product_price'] = $request->product_price;
         $data['product_desc'] = $request->product_desc;
-        
-        DB::table('tbl_product')->where('product_id',$product_id) ->update($data);
-        Session::put('message', 'Cập nhật danh mục sản phẩm thành công');
-        return Redirect::to('all-product');
+        $data['product_content'] = $request->product_content;
+        $data['category_id'] = $request->product_cate;
+        $data['brand_id'] = $request->product_brand;
+        $data['product_status'] = $request->product_status;
+
+        $get_image = $request->file('product_image');
+        if ($get_image) {
+            $get_name_image = $get_image->getClientOriginalName();
+            $name_image = current(explode('.', $get_name_image));
+            $new_image = $name_image . rand(0, 99) . '.' . $get_image->getClientOriginalExtension();
+            $get_image->move('public/uploads/product', $new_image);
+            $data['product_image'] = $new_image;
+        }
+
+        DB::table('tbl_product')->where('product_id', $product_id)->update($data);
+        Session::put('message', 'Cập nhật sản phẩm thành công');
+        return Redirect::to('/all-product');
     }
 
     public function delete_product_product($product_id){
