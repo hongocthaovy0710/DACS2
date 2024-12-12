82a1a2d307a0111335a55cc2bc68328a3bae2b08
hongocthaovy0710 <hongocthaovy66@gmail.com>
2024-12-13 00:14:32 +0700

    xong hết giờ chuyển nhánh làm mail

--- resources/views/admin/view_order.blade.php
+++ resources/views/admin/view_order.blade.php
@@ -121,19 +121,13 @@
                 <table class="table table-striped b-t b-light">
                     <thead>
                         <tr>
-                            <th style="width:20px;">
-                                <label class="i-checks m-b-none">
-                                    <input type="checkbox"><i></i>
-                                </label>
-                            </th>
+                            <th style="width:20px;"></th>
                             <th>Tên sản phẩm</th>
-                            <th>Số lượng kho còn</th>
                             <th>Mã giảm giá</th>
                             <th>Phí ship hàng</th>
                             <th>Số lượng</th>
                             <th>Giá sản phẩm</th>
                             <th>Tổng tiền</th>
-
                             <th style="width:30px;"></th>
                         </tr>
                     </thead>
@@ -149,10 +143,8 @@
                                 $total += $subtotal;
                             @endphp
                             <tr class="color_qty_{{ $details->product_id }}">
-
                                 <td><i>{{ $i }}</i></td>
                                 <td>{{ $details->product_name }}</td>
-                                <td>{{ $details->product->product_quantity }}</td>
                                 <td>
                                     @if ($details->product_coupon != 'no')
                                         {{ $details->product_coupon }}
@@ -161,29 +153,7 @@
                                     @endif
                                 </td>
                                 <td>{{ number_format($details->product_feeship, 0, ',', '.') }}đ</td>
-                                <td>
-
-                                    <input type="number" min="1" {{ $order_status == 2 ? 'disabled' : '' }}
-                                        class="order_qty_{{ $details->product_id }}"
-                                        value="{{ $details->product_sales_quantity }}" name="product_sales_quantity">
-
-                                    <input type="hidden" name="order_qty_storage"
-                                        class="order_qty_storage_{{ $details->product_id }}"
-                                        value="{{ $details->product->product_quantity }}">
-
-                                    <input type="hidden" name="order_code" class="order_code"
-                                        value="{{ $details->order_code }}">
-
-                                    <input type="hidden" name="order_product_id" class="order_product_id"
-                                        value="{{ $details->product_id }}">
-
-                                    @if ($order_status != 2)
-                                        <button class="btn btn-default update_quantity_order"
-                                            data-product_id="{{ $details->product_id }}" name="update_quantity_order">Cập
-                                            nhật</button>
-                                    @endif
-
-                                </td>
+                                <td>1</td>
                                 <td>{{ number_format($details->product_price, 0, ',', '.') }}đ</td>
                                 <td>{{ number_format($subtotal, 0, ',', '.') }}đ</td>
                             </tr>
@@ -203,7 +173,6 @@
                                     @php
                                         echo 'Tổng giảm :' . number_format($coupon_number, 0, ',', '.') . 'k' . '</br>';
                                         $total_coupon = $total + $details->product_feeship - $coupon_number;
-
                                     @endphp
                                 @endif
 
@@ -253,13 +222,11 @@
                                         </form>
                                     @endif
                                 @endforeach
-
-
                             </td>
                         </tr>
                     </tbody>
                 </table>
-                <a target="_blank" href="{{ url('/print-order/' . $details->order_code) }}">In đơn hàng</a>
+                {{-- <a target="_blank" href="{{ url('/print-order/' . $details->order_code) }}">In đơn hàng</a> --}}
             </div>
 
         </div>
